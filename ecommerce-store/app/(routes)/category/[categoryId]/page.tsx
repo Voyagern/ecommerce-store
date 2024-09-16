b64a515d9261e8bc72452a66c68b8332242d5233
Voyager_n <ol.turevici@gmail.com>
2024-09-16 20:10:56 +0300

    category screen add

--- ecommerce-store/app/(routes)/category/[categoryId]/page.tsx
+++ ecommerce-store/app/(routes)/category/[categoryId]/page.tsx
@@ -6,44 +6,52 @@ import getCategory from "@/actions/get-category";
 import Container from "@/components/ui/container";
 import Billboard from "@/components/billboard";
 import Filter from "./components/filter";
+import NoResults from "@/components/ui/no-results";
+import ProductCard from "@/components/ui/product-card";
+import MobileFilters from "@/app/(routes)/category/[categoryId]/components/mobile-filters";
 
+export const revalidate = 0;
 
-export const revalidate=0;
-interface CategoryPageProps{
-    params:{
-        categoryId:string
+interface CategoryPageProps {
+    params: {
+        categoryId: string
     },
-    searchParams:{
-        colorId:string
-        sizeId:string
+    searchParams: {
+        colorId?: string
+        sizeId?: string
     }
 }
-const CategoryPage:React.FC<CategoryPageProps> = async ({
-    params,
-    searchParams
-                                                  }) => {
 
-    const products=await getProducts({
-        categoryId:params.categoryId,
-        colorId:searchParams.colorId,
-        sizeId:searchParams.sizeId
-    })
+const CategoryPage: React.FC<CategoryPageProps> = async ({
+                                                             params,
+                                                             searchParams
+                                                         }) => {
 
-    const sizes=await getSizes();
-    const colors=await  getColors();
-    const category =await getCategory(params.categoryId);
+    const products = await getProducts({
+        categoryId: params.categoryId,
+        colorId: searchParams.colorId,
+        sizeId: searchParams.sizeId
+    });
+
+    const sizes = await getSizes();
+    const colors = await getColors();
+    const category = await getCategory(params.categoryId);
+
     return (
         <div className="bg-white">
             <Container>
-                <Billboard data={category.billboard}/>
+                {/* Ensure category.billboard exists */}
+                {category?.billboard && (
+                    <Billboard data={category.billboard} />
+                )}
                 <div className="px-4 sm:px-6 lg:px-8 pb-24">
                     <div className="lg:grid lg:grid-cols-5 lg:gap-x-8">
-                        {/* fillters*/}
+                        <MobileFilters sizes={sizes} colors={colors} />
                         <div className="hidden lg:block">
                             <Filter
-                            valueKey="sizeId"
-                            name="Sizes"
-                            data={sizes}
+                                valueKey="sizeId"
+                                name="Sizes"
+                                data={sizes}
                             />
                             <Filter
                                 valueKey="colorId"
@@ -51,11 +59,22 @@ const CategoryPage:React.FC<CategoryPageProps> = async ({
                                 data={colors}
                             />
                         </div>
+                        <div className="mt-6 lg:col-span-4 lg:mt-0">
+                            {products.length === 0 && <NoResults />}
+                            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+                                {products.map((item) => (
+                                    <ProductCard
+                                        key={item.id}
+                                        data={item}  // Pass actual product data here
+                                    />
+                                ))}
+                            </div>
+                        </div>
                     </div>
-
                 </div>
             </Container>
         </div>
     )
 }
-export default CategoryPage
+
+export default CategoryPage;
