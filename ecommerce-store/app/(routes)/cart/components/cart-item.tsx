14fa0b4bdf15b6c8002d0f2dfb217baf4dad4792
Voyager_n <ol.turevici@gmail.com>
2024-09-25 14:19:00 +0300

    changes2

--- ecommerce-store/app/(routes)/cart/components/cart-item.tsx
+++ ecommerce-store/app/(routes)/cart/components/cart-item.tsx
@@ -26,7 +26,7 @@ const CartItem: React.FC<CartItemProps> = ({
             <div className="relative h-24 w-24 rounded-md overflow-hidden sm:h-48 sm:w-48">
                 <Image
                     fill
-                    src={data.images[0].url}
+                    src={data.Images[0].url}
                     alt=""
                     className="object-cover object-center"
                 />
